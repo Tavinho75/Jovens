1a4961f5e5a876de14fe0ba110471b1dc47f8760
Tavinho75 <luisotavioreis75@gmail.com>
2025-02-26 10:33:13 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ function clickMenu() {
         itens.style.display = 'flex'
     }
 }
+
+document.getElementById("customForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let formData = new FormData(this);
+    let params = new URLSearchParams(formData).toString();
+    let googleFormURL = "https://docs.google.com/forms/d/e/1SsDv3Os0qwtbGkKRvBJwDEYYnDDpnffRmn-hbWjPFqw/formResponse";
+
+    fetch(googleFormURL, {
+        method: "POST",
+        body: params,
+        mode: "no-cors"
+    }).then(() => {
+        alert("Formulário enviado!");
+    }).catch(() => {
+        alert("Erro ao enviar.");
+    });
+});
